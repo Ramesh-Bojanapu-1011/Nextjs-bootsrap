ccd0ea823b04cef26c05b621a8a74786f59e7793
Ramesh Bojanapu <84425972+Ramesh-Bojanapu-1011@users.noreply.github.com>
2025-05-04 02:20:08 +0530

    feat: add tooltip functionality to Bootstrap components and update placeholder text

--- src/components/bootstrap-client.tsx
+++ src/components/bootstrap-client.tsx
@@ -4,7 +4,14 @@ import { useEffect } from "react";
 
 export default function BootstrapClient() {
   useEffect(() => {
-    import("bootstrap/dist/js/bootstrap.bundle.min.js");
+    import("bootstrap/dist/js/bootstrap.bundle.min.js").then(({ Tooltip }) => {
+      const tooltipTriggerList = document.querySelectorAll(
+        '[data-bs-toggle="tooltip"]',
+      );
+      [...tooltipTriggerList].forEach((tooltipTriggerEl) => {
+        new Tooltip(tooltipTriggerEl);
+      });
+    });
   }, []);
 
   return null;
